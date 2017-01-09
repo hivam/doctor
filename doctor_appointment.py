957a52f6cb5d920f395c5f7eb791a19891fab5b6
camilozuluaga <juancamilozuluagaserna@hotmail.com>
2017-01-09 15:10:29 -0500

    agregando cambios para poner las citas el appoitment en falso

--- doctor_appointment.py
+++ doctor_appointment.py
@@ -147,14 +147,11 @@ class doctor_appointment(osv.osv):
 			fecha_usuario_ini = fecha_hora_actual.strftime('%Y-%m-%d 00:00:00')
 			modulo_instalado = self.pool.get('ir.module.module').search(cr,uid,[('name', '=', 'doctor_multiroom'), ('state', '=', 'installed')],context=context)
 			if modulo_instalado:
-				_logger.info("entra si tiene multi room")
-				_logger.info(fecha_usuario_ini)
-				appointment_ids = self.search(cr, uid, [('time_begin', '<', fecha_usuario_ini), ('schedule_id', 'not in', [0]), ('state', '!=', 'cancel'), ('attended', '<>', True)], context=None)
-				_logger.info(appointment_ids)
+				appointment_ids = self.search(cr, uid, [('time_begin', '<', fecha_usuario_ini), ('schedule_id', 'not in', [0]), ('state', '!=', 'cancel'), ('attended', '<>', True), ('appointment_today', '=', True)], context=None)
 			else:
 				appointment_ids = self.search(cr, uid, [('time_begin', '<', fecha_usuario_ini), ('appointment_today', '=', True), ('attended', '<>', True)], context=None) 
 		
-		return super(doctor_appointment, self).write(cr, uid, appointment_ids, {'appointment_today': 'False', 'state': 'cancel'}, context=context)
+		return super(doctor_appointment, self).write(cr, uid, appointment_ids, {'appointment_today': 'False'}, context=context)
 	
 	def _check_appointment(self, cr, uid, ids, context=None):
 		for record in self.browse(cr, uid, ids, context=context):
